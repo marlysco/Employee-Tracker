c80a0dc7fed93d7e183ce51d53257320e66a78d1
marlysco <marlysco94@gmail.com>
2021-05-31 13:46:45 -0400

    Updating js file

--- schema.sql
+++ schema.sql
@@ -1,22 +1,23 @@
-DROP DATABASE IF EXISTS employee_tracker;
-CREATE DATABASE employee_tracker;
+DROP DATABASE IF EXISTS employee_dashboard;
+CREATE DATABASE employee_dashboard;
 
-USE employee_tracker;
+USE employee_dashboard;
 
-CREATE TABLE department (
- id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE departments (
+ id INT PRIMARY KEY NOT NULL,
  name VARCHAR(30) NOT NULL,
 
 
-CREATE TABLE role (
- id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles (
+ id INT PRIMARY KEY NOT NULL,
  title VARCHAR(30) NOT NULL,
  salary DECIMAL NOT NULL,
  department_id INT  NOT NULL);
  
- CREATE TABLE employee (
- id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+ CREATE TABLE employees (
+ id INT PRIMARY KEY NOT NULL,
  first_name VARCHAR(30) NOT NULL,
  last_name VARCHAR(30) NOT NULL,
  role_id INT  NOT NULL,
  manager_id INT  NOT NULL)
+
--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+
 //Setting with the sql database
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: 'root',
-  database: 'employee_tracker',
+  database: 'employee_dashboard',
 });
 
 console.log ("Welcome to the employee dashboard!");
@@ -27,7 +28,7 @@ const allOptions = () => {
         choices: ["View deparments, roles or employees", "Add deparments, roles or employees", "Update employee roles", "Update employee managers", "View employees by manager", "Delete departments, roles, or employees", "View the total utilized budget of a department"]
     },
     ]).then((answer)=>{
-        switch(answer.choices) {
+        switch(answer.view) {
             case "View deparments, roles or employees":
               view();
               break;
@@ -48,39 +49,163 @@ const allOptions = () => {
               break;
             case "View the total utilized budget of a department":
               budget();
-              break;       
+              break;  
+      }   
+        
         }
-    })
+
+    )
+   
 }
 
 allOptions();
 
-const view = () => {
-    inquirer. prompt([
-        {
-            name: 'view',
+
+// First case: Show
+const view =()=> {
+    console.log("test")
+    inquirer.prompt([
+        {   
+            name: 'show',
             type: 'list',
             message: 'Please select what information you want to display: ',
-            choices: ["Deparments", "Roles", "Employees"]
+            choices: ["Departments", "Roles", "Employees"]
         },   
     ]).then((answer)=>{
-        switch(answer.choices) {
-            case "Deparments":
-                show(department);
-              break;
+        switch(answer.show) {
+            case "Departments":
+               showDepartments();
+                break;
             case "Roles":
-                show(role);
-              break;
+                showRoles();
+             break;
             case "Employees":
-                show(employee);
-              break;
+                showEmployees();
+            break;
         }
     })
 }
 
-const show = () => {
-    connection.query( 'SELECT * FROM ?',
-        (err) => {
-          if (err) throw err;
+
+//Second case: Add
+const add=()=> {
+    inquirer. prompt([
+        {
+            name: 'add',
+            type: 'list',
+            message: 'What do you want to add to the dashboard',
+            choices: ["Deparments", "Roles",  "Employees"]
+        },
+        ]).then((answer)=>{
+            switch(answer.add) {
+                case "Employees":
+                 addEmployee();
+                break;
+                case "Roles":
+                 addRoles();
+                break;
+                case "Departments":
+                 addDepartment();
+            }
         })
-}
+ }
+
+
+
+ // First case subfunctions
+ const showDepartments=()=>{
+    connection.query( 'SELECT * FROM departments ORDER BY id DESC',
+    (err, res) => {
+    if (err) throw err;
+    console.log ("ID  | Name")
+    console.log("-------------------------")
+    res.forEach(({ id, name}) => {
+        console.log(`${id} | ${name}`);
+        console.log("......................")
+      });
+             })
+ };
+
+ const showRoles=()=>{
+    connection.query( 'SELECT * FROM roles ORDER BY id DESC',
+    (err, res) => {
+    if (err) throw err;
+    console.log ("ID  |     Title     |     Salary    |   Department ID")
+    console.log("----------------------------------------------------------")
+    res.forEach(({ id, title, salary, department_id}) => {
+        console.log(`${id} | ${title}  |  $${salary}.00 |    ${department_id}`); 
+      });
+     })
+ }
+
+ const showEmployees=()=>{
+    connection.query( 'SELECT * FROM employees ORDER BY id DESC',
+    (err, res) => {
+    if (err) throw err;
+    console.log ("ID  |       Name       |   Role ID    |  Manager ID ")
+    console.log("---------------------------------------------------------------------------")
+    res.forEach(({ id, first_name, last_name, role_id, manager_id}) => {
+        console.log(`${id} | ${first_name} ${last_name} |  ${role_id}  | ${manager_id}`); 
+      });
+     })
+ }
+
+
+
+ //Second case subfunctions
+ const addEmployee = () =>{
+    connection.query('SELECT title FROM roles', (err,res)=>{
+        if(err) throw err;
+    inquirer.prompt([        
+        {
+           name: 'firstName',
+           type: 'input',
+           message: 'Please enter the employee first name',
+        },
+        {
+           name: 'secondName',
+           type: 'input',
+           message: 'Please enter the employee second name',
+        },
+        {
+           name: 'role',
+           type: 'rawlist',
+           message: 'Please select the role of the employee',
+           choices() {
+            const roles = [];
+            res.forEach(({ title }) => {
+              roles.push(`${title}`);
+            });
+            return roles;
+          },
+        },
+        {
+           name: 'id',
+           type: 'input',
+           message: 'Please enter the employee id',
+        }   
+    ]).then((answer)=>{
+      //Query to get the role id from the selected role
+      connection.query('SELECT r.id FROM roles r JOIN employees e ON r.id = e.role_id AND title=?;', answer.role, (err, res)=> {
+        if(err) throw err;
+        const resId=res;
+        console.log(resId); 
+     //Query to get the manager id from the selected role
+      connection.query('SELECT manager_id FROM employees WHERE role_id=?;', resId, (err, res)=>{
+        if(err) throw err;
+         const managerId=res;
+         console.log(managerId);
+    //Query to insert all the values in the new employee record
+      connection.query('INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES ?;',
+      {
+       role_id:resId,
+       id:answer.id,
+       first_name: answer.firstName,
+       last_name: answer.secondName,
+       manager_id:managerId,
+      })
+    })
+   }) 
+ })
+})
+ }
